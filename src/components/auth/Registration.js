8a9d47e093c27750057af3271aaabe3d7d5ad206
BableshAAzad <bableshaazad@outlook.com>
2023-06-18 12:40:54 +0530

    add registration form

--- src/components/auth/Registration.js
+++ src/components/auth/Registration.js
@@ -1,17 +1,43 @@
 import { TextField, Button, Box, Alert } from '@mui/material';
 import { useState } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Registration = () => {
+    const [error, setError] = useState({
+        status: false,
+        msg: '',
+        type: ''
+    })
+    const navigate = useNavigate();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const data = new FormData(e.currentTarget);
+        const actualData = {
+            name: data.get('name'),
+            email: data.get('email'),
+            password: data.get('password'),
+            password_confirmation: data.get('password_confirmation')
+
+        }
+        if (actualData.email && actualData.password) {
+            console.warn(actualData);
+            document.getElementById('login-form').reset();
+            setError({ status: true, msg: 'Login Success', type: 'success' });
+            navigate('/');
+        } else {
+            setError({ status: true, msg: 'All Fields are Required', type: 'error' })
+        }
+    }
     return (
         <>
-            <Box component='form' noValidate sx={{ mt: 1 }} id='login-form' onSubmit={handleSubmit}>
-                <TextField margin='normal' required fullWidth id='email' name='email' label='Email Address' autoFocus />
+            <Box component='form' noValidate sx={{ mt: 1 }} id='registration-form' onSubmit={handleSubmit}>
+                <TextField margin='normal' required fullWidth id='name' name='name' label='Name' autoFocus />
+                <TextField margin='normal' required fullWidth id='email' name='email' label='Email Address'/>
                 <TextField margin='normal' required fullWidth id='password' name='password' label='Password' type='password' />
+                <TextField margin='normal' required fullWidth id='password_confirmation' name='password_confirmation' label='Confirm Password' type='password' />
                 <Box textAlign='center'>
-                    <Button type='submit' variant='contained' sx={{ mt: 3, mb: 2, px: 5 }}>Login</Button>
+                    <Button type='submit' variant='contained' sx={{ mt: 3, mb: 2, px: 5 }}>Join</Button>
                 </Box>
-                <NavLink to='/'>Forgot Password ?</NavLink>
                 {error.status ? <Alert severity={error.type} sx={{ mt: 1 }}>{error.msg}</Alert> : ''}
             </Box>
         </>
