5b9bdb553131a0e6f948178481cc454c5ec8666a
BableshAAzad <bableshaazad@outlook.com>
2023-06-20 21:42:13 +0530

    add dashboard data

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,22 +1,28 @@
+import { Grid } from "@mui/material";
+
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <hr />
-      <p>
-        Contact Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        Similique deserunt adipisci sunt quia quae accusantium ullam omnis ipsa
-        suscipit totam officiis, quaerat aut natus soluta esse voluptatibus
-        perspiciatis explicabo, molestiae veniam reiciendis laudantium tenetur
-        eveniet. Quo est fugit eos, optio inventore hic alias. Itaque quam
-        repellat nihil necessitatibus id eius, voluptas, omnis dolore est libero
-        quaerat fugit a quos minus accusantium sequi accusamus cum ratione
-        aliquam. Assumenda sapiente adipisci cum rerum iusto veritatis sequi
-        harum, minus magni molestias facere optio quisquam soluta nostrum hic
-        ullam, sit nesciunt impedit. Provident est perspiciatis nulla optio
-        distinctio ea consequatur iste quasi soluta. Harum?
-      </p>
-    </div>
+    <>
+      <Grid containet justifyContent='center'>
+        <Grid item sm={10}>
+        <h1>Contact Page</h1>
+        <hr />
+        <p>
+          Contact Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Similique deserunt adipisci sunt quia quae accusantium ullam omnis ipsa
+          suscipit totam officiis, quaerat aut natus soluta esse voluptatibus
+          perspiciatis explicabo, molestiae veniam reiciendis laudantium tenetur
+          eveniet. Quo est fugit eos, optio inventore hic alias. Itaque quam
+          repellat nihil necessitatibus id eius, voluptas, omnis dolore est libero
+          quaerat fugit a quos minus accusantium sequi accusamus cum ratione
+          aliquam. Assumenda sapiente adipisci cum rerum iusto veritatis sequi
+          harum, minus magni molestias facere optio quisquam soluta nostrum hic
+          ullam, sit nesciunt impedit. Provident est perspiciatis nulla optio
+          distinctio ea consequatur iste quasi soluta. Harum?
+        </p>
+      </Grid>
+    </Grid >
+    </>
   );
 };
 
